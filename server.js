0f13272349abf7f05efaaac25c280028e922bd12
jonathan-luscap <jonathan.luscap@le-campus-numerique.fr>
2024-04-10 12:04:06 +0200

    iteration 5 completed

--- server.js
+++ server.js
@@ -14,3 +14,10 @@ app.get('/', (req, res) => {
     console.log("Got it!");
     res.send('Hello World!');
 });
+
+app.use( function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    next();
+});
