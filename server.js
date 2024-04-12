de72fd35cdc7256fd2f3403ea771dc4fb2c5e391
jonathan-luscap <jonathan.luscap@le-campus-numerique.fr>
2024-04-12 13:36:25 +0200

    user can create folder {name} in {folder}

--- server.js
+++ server.js
@@ -51,22 +51,85 @@ app.get('/api/drive', async (req, res) => {
         const directory = await fs.promises.readdir(rootPath, {withFileTypes:true});
         const dirFormated = directory.map(item => ({
             name: item.name,
-            isDirectory: item.isDirectory(),
+            isFolder: item.isDirectory(),
         }))
-        res.send(dirFormated);
+        res.status(200).send(dirFormated);
     } catch (e) {
         console.error('Error retrieving directory contents ', e);
         res.status(500).send('Error while retrieving directory contents');
     }
 });
 
+
+app.get('/api/drive/:driveId', async (req, res) => {
+    console.log(req.params);
+    const targetName = req.params.driveId;
+    const regex = /[a-zA-Z0-9]/;
+    if (regex.test(targetName)) {
+        const relativePath = path.join(rootPath, targetName);
+        try {
+            const directory = await fs.promises.readdir(relativePath, {withFileTypes:true});
+            const dirFormated = directory.map(item => ({
+                name: item.name,
+                isFolder: item.isDirectory(),
+            }))
+            res.status(200).send(dirFormated);
+            console.log(dirFormated);
+        } catch (e) {
+            console.error('Error retrieving directory contents ', e);
+            res.status(404).send('Error the directory does not exist');
+        }
+    }
+
+});
+
+
+
+
+
 app.post('/api/drive', async (req, res) => {
     const { name } = req.query;
-    try{
-        const folderPath = path.join(rootPath, name);
-        await fs.promises.mkdir(folderPath, { recursive: true });
-        return res.sendStatus(201);
-    } catch (e) {
-        return res.status(500).send(`Cannot create the folder: ${e}`);
+    const regex = /[a-zA-Z0-9]/;
+    if (regex.test(name)) {
+        try{
+            const folderPath = path.join(rootPath, name);
+            await fs.promises.mkdir(folderPath, { recursive: true });
+            return res.sendStatus(201);
+        } catch (e) {
+            return res.status(500).send(`Cannot create the folder: ${e}`);
+        }
+    } else {
+        return res.status(400).send('Not found');
+    }
+});
+
+app.post('/api/drive/:driveId', async (req, res) => {
+    const { driveId } = req.params;
+    const relativePath = path.join(rootPath, driveId);
+    console.log(relativePath)
+    const { name } = req.query;
+    console.log(name);
+    const regex = /[a-zA-Z0-9]/;
+    fs.stat(relativePath, (err, stats) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                console.log(`Directory ${driveId} does not exist`);
+                res.status(404).send();
+            } else {
+                console.log(`Error accessing directory ${driveId}.`);
+                res.status(500).send();
+            }
+        }
+    });
+    if (regex.test(name)) {
+        try {
+            const folderPath = path.join(relativePath, name);
+            await fs.promises.mkdir(folderPath, { recursive: true });
+            return res.sendStatus(201);
+        } catch (e) {
+            return res.status(500).send(`Cannot create the folder ${name} : ${e}`);
+        }
+    } else {
+        return res.status(400).send(`${name} can't be created : wrong format.`);
     }
 });
