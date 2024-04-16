dc8dc4e74873ddaf896c5b027e2a31319c6f18c3
jonathan-luscap <jonathan.luscap@le-campus-numerique.fr>
2024-04-16 16:46:31 +0200

    fin de journée

---
+++ functions.js
@@ -0,0 +1,59 @@
+const fs = require('fs');
+
+function createTempPath(path) {// crée le fichier temporaire racine
+    try {
+        fs.mkdir(path, err => {
+            if (err) throw err;
+        });
+        console.log(`Rootpath exists : ${path}`);
+        return true;
+    } catch (e) {
+        console.error('Error while creating the folder rootpath in temp directory : ', e);
+        return false;
+    }
+}
+
+function formatDirent(dirents) {
+    const formated = dirents.map(item => ({
+        name: item.name,
+        isFolder: item.isDirectory(),
+    }))
+    return formated;
+}
+
+function testFolderNameRegex(name) {
+    const regex = /[^a-zA-Z0-9]/
+    if (regex.test(name)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function testFileNameRegex(name) {
+    const regex = /^[^\\/:\*\?"<>\|]+$/;
+    if (regex.test(name)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+async function isPathExisting(path) {
+    try {
+        await fs.promises.access(path, fs.constants.R_OK & fs.constants.W_OK);
+        console.log(`Directory ${path} exists`);
+        return true;
+    } catch (e) {
+        console.log(`Directory ${path} does not exist`);
+        return false;
+    }
+}
+
+module.exports = {
+    createTempPath,
+    formatDirent,
+    testFolderNameRegex,
+    testFileNameRegex,
+    isPathExisting,
+};
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const {createTempPath, formatDirent, testFolderNameRegex, testFileNameRegex, isPathExisting} = require("./functions");
+
 
 const bb = require('express-busboy');
 bb.extend(app, {
@@ -10,55 +12,35 @@ bb.extend(app, {
 const fs = require('fs');
 const os = require('os');
 const path = require('path');
+const test = require("node:test");
 const port = 3000;
 
-app.use( function (req, res, next) {
-
+// permet de traiter les CORS (crossing-origin request)
+app.use(function (req, res, next) {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', '*');
     res.setHeader("Access-Control-Allow-Headers", "*");
     next();
 });
 
-function start () {
+function start() {
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     })
 }
 
-module.exports = start;
-
-let pathAlreadyExists = false;
+module.exports = start;// nécesaire pour exporter la fonction start
 
 const rootPath = path.join(os.tmpdir(), 'alps_drive');
 
 if (!fs.existsSync(rootPath)) {
-    try{
-        fs.mkdirSync(rootPath);
-        console.log(`Directory ${rootPath} exists`);
-        pathAlreadyExists = true;
-    } catch(e){
-        console.error('Error while creating the folder rootpath in temp directory : ', e);
-    }
-} else {
-    if(!pathAlreadyExists){
-        console.log('Rootpath already exists');
-        pathAlreadyExists = true;
-    }
+    createTempPath(rootPath);
 }
-
-app.get('/', (req, res) => {
-    console.log("Got it!");
-    res.send('Hello World!');
-});
 
 app.get('/api/drive', async (req, res) => {
     try {
-        const directory = await fs.promises.readdir(rootPath, {withFileTypes:true});
-        const dirFormated = directory.map(item => ({
-            name: item.name,
-            isFolder: item.isDirectory(),
-        }))
+        const directory = await fs.promises.readdir(rootPath, {withFileTypes: true});
+        const dirFormated = formatDirent(directory);
         res.status(200).send(dirFormated);
     } catch (e) {
         console.error('Error retrieving directory contents ', e);
@@ -66,109 +48,88 @@ app.get('/api/drive', async (req, res) => {
     }
 });
 
-
-app.get('/api/drive/:driveId', async (req, res) => {
-    console.log(req.params);
-    const targetName = req.params.driveId;
-    const regex = /[a-zA-Z0-9]/;
-    if (regex.test(targetName)) {
-        const relativePath = path.join(rootPath, targetName);
-        try {
-            const directory = await fs.promises.readdir(relativePath, {withFileTypes:true});
-            const dirFormated = directory.map(item => ({
-                name: item.name,
-                isFolder: item.isDirectory(),
-            }))
-            res.status(200).send(dirFormated);
-            console.log(dirFormated);
-        } catch (e) {
-            console.error('Error retrieving directory contents ', e);
-            res.status(404).send('Error the directory does not exist');
-        }
+app.get('/*', async (req, res) => {
+    const hypoPath = req.params[0];
+    const realPath = hypoPath.replace("api/drive/", "");
+    try {
+        const folderPath = path.join(rootPath, realPath);
+        const directory = await fs.promises.readdir(folderPath, {withFileTypes: true});
+        const dirFormated = formatDirent(directory);
+        res.status(200).send(dirFormated);
+    } catch (e) {
+        console.error('Error retrieving directory contents ', e);
+        res.status(404).send('Error the directory does not exist');
     }
 
 });
 
-
-
-
-
 app.post('/api/drive', async (req, res) => {
-    const { name } = req.query;
-    const regex = /[a-zA-Z0-9]/;
-    if (regex.test(name)) {
-        try{
+    const {name} = req.query;
+    if (testFolderNameRegex(name)) {
+        try {
             const folderPath = path.join(rootPath, name);
-            await fs.promises.mkdir(folderPath, { recursive: true });
+            await fs.promises.mkdir(folderPath, {recursive: true});
             return res.sendStatus(201);
         } catch (e) {
             return res.status(500).send(`Cannot create the folder: ${e}`);
         }
     } else {
-        return res.status(400).send('Not found');
+        return res.status(400).send('Wrong folder name!');
     }
 });
 
-app.post('/api/drive/:driveId', async (req, res) => {
-    const { driveId } = req.params;
-    const relativePath = path.join(rootPath, driveId);
-    const { name } = req.query;
-    const regex = /[a-zA-Z0-9]/;
-    fs.stat(relativePath, (err, stats) => {
-        if (err) {
-            if (err.code === 'ENOENT') {
-                console.log(`Directory ${driveId} does not exist`);
-                res.status(404).send();
-            } else {
-                console.log(`Error accessing directory ${driveId}.`);
-                res.status(500).send();
-            }
-        }
-    });
-    if (regex.test(name)) {
+app.post('/*', async (req, res) => {
+    const hypoPath = req.params[0];
+    const realPath = hypoPath.replace("api/drive/", "");
+    const {name} = req.query;
+    const folderPath = path.join(rootPath, realPath, name);
+    if (!isPathExisting(folderPath)) {
+        res.sendStatus(404);
+        return;
+    }
+    if (testFileNameRegex(name)) {
         try {
-            const folderPath = path.join(relativePath, name);
-            await fs.promises.mkdir(folderPath, { recursive: true });
+            await fs.promises.mkdir(folderPath, {recursive: true});
             return res.sendStatus(201);
         } catch (e) {
             console.log(`Cannot create the folder ${name} : ${e}`)
-            return res.status(500).send();
+            return res.sendStatus(500);
         }
     } else {
         console.log(`${name} can't be created : wrong format.`)
-        return res.status(400).send();
+        return res.sendStatus(400);
     }
 });
 
-app.delete('/api/drive/:driveId', async (req, res) => {
-    const { driveId } = req.params;
-    const regex = /[a-zA-Z0-9]/;
-    if (regex.test(driveId)){
-        try{
-            const relativePath = path.join(rootPath, driveId);
-            await fs.promises.rm(relativePath, { recursive: true });
-            console.log(`${driveId} has been removed.`)
+app.delete('/*', async (req, res) => {
+    const hypoPath = req.params[0];
+    const realPath = hypoPath.replace("api/drive/", "");
+    if (testFolderNameRegex(realPath)) {
+        try {
+            const folderPath = path.join(rootPath, realPath);
+            console.log('folderPath : ' + folderPath);
+            await fs.promises.rm(folderPath, {recursive: true});
+            console.log(`${realPath} has been removed.`)
             return res.sendStatus(200);
         } catch (e) {
-            console.log(`Cannot remove the folder ${driveId} : ${e}`)
+            console.log(`Cannot remove the folder ${realPath} : ${e}`)
             return res.status(500)
         }
     } else {
-        console.log(`${driveId} can't be removed : wrong format.`);
+        console.log(`${realPath} can't be removed : wrong format.`);
         return res.status(400);
     }
 });
 
-app.delete('/api/drive/:driveId', async (req, res) => {
-    const { driveId } = req.params;
-    const regex = /[a-zA-Z0-9]/;
-    const { name } = req.query;
-    if (regex.test(driveId)){
-        const relativePath = path.join(rootPath, driveId);
-        if (regex.test(name)){
-            try{
-                const folderPath = path.join(relativePath, name);
-                await fs.promises.rm(folderPath, { recursive: true });
+app.delete('/*', async (req, res) => {
+    const hypoPath = req.params[0];
+    const {name} = req.query;
+    const realPath = hypoPath.replace("api/drive/", "");
+    if (isPathExisting(realPath)) {
+        if (testFolderNameRegex(name)) {
+            try {
+                const targetPath = path.join(rootPath, realPath, name);
+                await fs.promises.rm(targetPath, {recursive: true});
                 console.log(`${name} has been removed.`)
                 res.status(200);
             } catch (e) {
@@ -180,46 +141,43 @@ app.delete('/api/drive/:driveId', async (req, res) => {
             return res.status(400)
         }
     }
-
 });
 
 app.put('/api/drive', async (req, res) => {
     const file = req.files.file;
-    if (typeof file !== 'undefined'){
+    if (!file) {
+        return res.sendStatus(400)
+    }
+    try {
         const filePath = path.join(rootPath, file.filename);
-        try {
-            await fs.rename(file.file, filePath, (err) => {
-                if (err) throw err;
-            })
-            return res.sendStatus(201)
-        } catch (e) {
-            console.log(e)
-            return res.sendStatus(500);
-        }
-    } else {
-        return res.sendStatus(400);
+        await fs.promises.rename(file.file, filePath)
+        return res.sendStatus(201)
+    } catch (e) {
+        console.log(e)
+        return res.sendStatus(500);
     }
 });
 
 app.put('/api/drive/:driveId', async (req, res) => {
-    const { driveId } = req.params;
+    const {driveId} = req.params;
+    const driveIdPath = path.join(rootPath, driveId)
     const file = req.files.file;
-    if (typeof driveId !== 'undefined') {
-        if (typeof file !== 'undefined'){
-            const filePath = path.join(rootPath, driveId, file.filename);
-            try {
-                await fs.rename(file.file, filePath, (err) => {
-                    if (err) throw err;
-                })
-                return res.sendStatus(201)
-            } catch (e) {
-                console.log(e)
-                return res.sendStatus(500);
-            }
-        } else {
-            return res.sendStatus(400);
-        }
-    } else {
+
+    const isExisting = await isPathExisting(driveIdPath);
+    if (!isExisting) {
         return res.sendStatus(404);
+    }
+
+    if (!file) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const filePath = path.join(driveIdPath, file.filename);
+        await fs.promises.rename(file.file, filePath);
+        return res.sendStatus(201)
+    } catch (e) {
+        console.log(e)
+        return res.sendStatus(500);
     }
 });
